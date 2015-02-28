ad19897d3231a6df8af5cde76c552aa34bee584c
Matt Karl <matt@cloudkid.com>
2015-02-27 23:13:34 -0500

    Fixed download

--- app/routes/download.js
+++ app/routes/download.js
@@ -1,32 +1,44 @@
 var router = require('express').Router();
 var Sound = require('../models/sound');
 
-router.get('/:uri', function(req, res)
+router.get('/:uri/:type(original|mp3|ogg)', function(req, res)
 {
-    var uri = req.params.uri;
-    var ampIndex = uri.indexOf('&');
-    var type = uri.substr(uri.length - 4);
-    var assetId = uri.substr(0, ampIndex);
-    var path = './public/sounds/%FOLDER%' + assetId + type;
-    switch (type)
-    {
-        case '.aif':
-            path = path.replace('%FOLDER%', 'originals/');
-            break;
-        case '.mp3':
-        case '.ogg':
-            path = path.replace('%FOLDER%', 'web/');
-            break;
-    }
+	Sound.getByUri(req.params.uri, function(err, sound)
+	{
+		if (!sound)
+		{
+			res.redirect('/');
+		}
+		else
+		{
+			var ext, path;
 
-    var fileName = uri.substr(ampIndex + 1);
-    res.download(path, fileName, function(err)
-    {
-        if (err)
-        {
-            console.log("No file at path ".red + path);
-        }
-    });
+			// Select which download to use
+			switch(req.params.type)
+			{
+				case 'original' : 
+					ext = sound.type;
+					path = 'original'; 
+					break;
+				case 'mp3':
+				case 'ogg': 
+					ext = req.params.type;
+					path = 'web'; 
+					break;
+			}
+			
+			// Download the file
+			var source = './public/sounds/' + path + '/' + sound.assetId + '.' + ext;
+			var target = sound.uri + '.' + ext;
+			res.download(source, target, function(err)
+			{
+				if (err)
+				{
+					console.log("No file at path ".red + source);
+				}
+			});
+		}
+	});
 });
 
 module.exports = router;
