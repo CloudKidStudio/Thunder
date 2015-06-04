aba34ab5ebbb78421971e24cc81fde143bcd5568
Matt Karl <matt@cloudkid.com>
2015-06-04 13:22:14 -0400

    Added CI support

--- tasks/aliases.js
+++ tasks/aliases.js
@@ -3,4 +3,5 @@ module.exports = function(grunt)
     grunt.registerTask('default', ['build', 'copy:libraries']);
     grunt.registerTask('debug', ['build-debug', 'copy:libraries']);
     grunt.registerTask('dev', ['express:dev', 'watch']);
+    grunt.registerTask('test', ['build']);
 };
